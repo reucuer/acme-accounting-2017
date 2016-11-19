d9d843b788ff11f2a12682eb44d6f273f6808bb7
Stutednt of ISU <lab@irnok.net>
2016-11-20 03:47:43 +0800

    Initial import

---
+++ components.py
@@ -0,0 +1,26 @@
+from interfaces import IAccountingEntry, IDocument, IStorage
+from zope.interface import implementer
+import datetime
+
+# Implementation.
+
+@implementer(IAccountingEntry)
+class Entry(object):
+    def __init__(self, cr, dr, amount):
+        self.cr=cr
+        self.dr=dr
+        self.amount=amount
+
+class Document(object):
+    def __init__(self, number, date):
+        self.number=number
+        self.date=date
+
+@implementer(IDocument)
+class CreditSlip(Document):
+    def __init__(self, number, date):
+        super(CreditSlip, self).__init__(number, date)
+        self.entries=[]
+
+    def addentry(self, entry):
+        self.entries.append(entry)
---
+++ interfaces.py
@@ -0,0 +1,31 @@
+from zope.interface import Interface, Attribute
+
+# Implementation - реализация
+# Provide - обеспечить, **обслуживать**, оснащать.
+
+
+class IAccountingEntry(Interface):
+    cr=Attribute("Credit account")
+    dr=Attribute("Debit account")
+    amount=Attribute("Amount of money sent")
+
+class IDocument(Interface):
+    def create():
+        """
+        Create document.
+        """
+    def delete():
+        """
+        Delete the document.
+        """
+    def save():
+        """
+        Store the document in a global storage,
+        and create a set of accounting entries.
+        """
+
+class IStorage(Interface):
+    def store(document):
+        """
+        Store the document in storage
+        """
